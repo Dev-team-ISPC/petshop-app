af9d360f4a953ec7326f2d2f65f8d01bd0a0bf68
ElizabethChiava-22 <chialvaelizabeth@gmail.com>
2026-05-09 12:17:36 -0300

    formulario contacto con FormBuilder

--- frontend/src/app/pages/contact/contact.ts
+++ frontend/src/app/pages/contact/contact.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group(
+{
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]]
+});
+}
+onEnviar(event: Event) {
+  console.log(this.form.value);
+}
+}
