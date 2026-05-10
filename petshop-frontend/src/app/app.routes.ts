82a558f7e2101a3ab9847e1ba9b614012cc08d2d
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2026-05-10 18:24:40 -0300

    implementacion  sección Quienes Somos y mejoraras  UX del frontend

--- petshop-frontend/src/app/app.routes.ts
+++ petshop-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './page/home/home';
 import { Productos } from './page/productos/productos';
 import { Servicios } from './page/servicios/servicios';
 import { Contacto } from './page/contacto/contacto';
+import { QuienesSomos } from './page/quienes-somos/quienes-somos';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'productos', component: Productos },
   { path: 'servicios', component: Servicios },
   { path: 'contacto', component: Contacto },
+  { path: 'quienes-somos', component: QuienesSomos },
   { path: '**', redirectTo: '' }
 ];
