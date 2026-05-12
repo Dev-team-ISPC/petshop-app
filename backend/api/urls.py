6dfeb690f65eb96685635ce5fcb86c09ffd228d1
nicolas <nicolasaudicio95@gmail.com>
2026-05-12 19:43:44 -0300

    Stop tracking virtual environments

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UsuarioViewSet, home
+from .views import (home, UsuarioViewSet, MascotaViewSet, TurnoViewSet, VacunacionViewSet, VacunaViewSet,
+                    CategoriaViewSet,ProductoViewSet)
 
-router = DefaultRouter()
-router.register(r'usuarios', UsuarioViewSet)
+
+
 
 urlpatterns = [
     path('', home),
-    path('', include(router.urls)),
+    path('usuarios/', UsuarioViewSet.as_view()),
+    path('mascotas/', MascotaViewSet.as_view()),
+    path('turnos/', TurnoViewSet.as_view()),
+    path('vacunaciones/', VacunacionViewSet.as_view()),
+    path('vacunas/', VacunaViewSet.as_view()),
+    path('categorias/', CategoriaViewSet.as_view()),
+    path('productos/', ProductoViewSet.as_view()),
 ]
