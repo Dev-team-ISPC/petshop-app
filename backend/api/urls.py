b0b184f1e9d6ba4f57288a496ec56fa28846ecd9
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2026-05-31 23:26:34 -0300

    agrego path login

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, home
+from .views import UserViewSet, home, login
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', home),
+    path('login/', login),
     path('', include(router.urls)),
 ]
