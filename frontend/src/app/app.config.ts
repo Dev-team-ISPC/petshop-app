47b6f7175552364f778aad7e951cddb7fb8a8281
ElizabethChiava-22 <chialvaelizabeth@gmail.com>
2026-06-03 16:31:52 -0300

    resolver conflictos merge main en develop

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,23 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-<<<<<<< HEAD
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient()
-=======
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(),
->>>>>>> origin/main
   ]
 };
